6447b649e629b3604cdd71f1adf9fffdf5431a6e
kento_tomita_zitaku <printf89@gmail.com>
2020-09-05 22:26:19 +0900

    もろもろ追加

---
+++ InkoP/Assets/Script/Title/Titlescene.cs
@@ -0,0 +1,27 @@
+﻿using UniRx;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Titlescene : MonoBehaviour
+{
+    [SerializeField] private Button _anyButton;
+
+    private void Awake()
+    {
+        //画面全体を押したとき
+        _anyButton.onClick.AsObservable().Subscribe(_ 
+            => 
+        　　{
+              SceneManager.LoadScene ("green");
+            }).AddTo(this);
+    }
+
+    private void SetEvent()
+    {
+    }
+
+    private void SetBind()
+    {
+    }
+}
