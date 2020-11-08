b01c4ed2f8f81bf1c598df85ff45343ff22c9275
kento_tomita_zitaku <printf89@gmail.com>
2020-11-08 23:01:25 +0900

    階層移動

---
+++ InkoP/Assets/Inko/Script/JoystickEvent/JoysitickManager.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace Assets.Inko.Script.JoystickEvent
+{
+  public class JoysitickManager : SingletonMonoBehaviour<JoysitickManager>
+  {
+    public float Horizontal;
+    public float Vertical;
+  }
+}
